3d4c563df96347314ff8a58792c82d3acce25ebc
reneab <rene.abd@gmail.com>
2019-04-06 15:02:45 +0800

    remove class attribute declaration

--- src/app/ScheduleEntry.ts
+++ src/app/ScheduleEntry.ts
@@ -1,10 +1,4 @@
 export class ScheduleEntry {
-    // id: number;
-    // time: string;
-    // teacher: string;
-    // batch: string;
-    // room: string;
-    // subject: string;
 
     constructor(public id: string, public time: string, public teacher: string,
                 public batch: string, public room: string, public subject: string) {}
