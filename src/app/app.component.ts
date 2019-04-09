5ef4b6a22471cbe5819c09953678c17ad75c88d4
reneab <rene.abd@gmail.com>
2019-04-09 15:47:04 +0800

    clean app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from './data.service';
-import { ScheduleEntry } from './ScheduleEntry';
-import { MatDialog } from '@angular/material';
-import { ErrorMessageDialogComponent } from './error-message-dialog/error-message-dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -11,42 +7,9 @@ import { ErrorMessageDialogComponent } from './error-message-dialog/error-messag
 })
 export class AppComponent implements OnInit {
   title = 'schedulator';
-  scheduleData: any;
-  // empty initialization before settings arrive from back-end
-  settings = {timeslots: [], batches: [], rooms: [], teachers: []};
 
-  constructor(private dataService: DataService, public dialog: MatDialog) { }
+  constructor() { }
 
-  ngOnInit(): void {
-    this.dataService.getAllSettings().subscribe(data => {
-      this.settings = data;
-    });
-    this.refreshSchedules();
-  }
+  ngOnInit(): void { }
 
-  // reformat schedule data per batch, to fit in the schedule grid
-  // TODO later parametrize this one to be able to filter by teacher and room
-  private formatSchedule = function(entries: ScheduleEntry[]) {
-    return this.settings.timeslots.map((t: string) => {
-      const filtered = entries.filter(d => d.time === t);
-      const final = this.settings.batches.map(b => filtered.find(e => e.batch === b));
-      return {time: t, data: final};
-    });
-  };
-
-  private refreshSchedules = function() {
-    console.log('Loading schedules...');
-    this.dataService.getAllSchedules().subscribe(data => {
-      const arr: ScheduleEntry[] = data.map(e => new ScheduleEntry(e._id, e.time, e.teacher, e.batch, e.room, e.subject));
-      this.scheduleData = this.formatSchedule(arr);
-    });
-  };
-
-  openErrorDialog(message: string): void {
-    this.dialog.open(ErrorMessageDialogComponent, {
-      height: '180px',
-      width: '350px',
-      data: {errorMessage: message}
-    });
-  }
 }
