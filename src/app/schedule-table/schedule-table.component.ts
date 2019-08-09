3928c374554f0f320b4981a498997160b840420f
reneab <rene.abd@gmail.com>
2019-08-09 14:53:46 +0800

    remove useless imports

--- src/app/schedule-table/schedule-table.component.ts
+++ src/app/schedule-table/schedule-table.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { DataService } from '../data.service';
 import { MatDialog } from '@angular/material';
 import { ErrorMessageDialogComponent } from '../error-message-dialog/error-message-dialog.component';
-import { ScheduleEntry } from '../ScheduleEntry';
-import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
-import { SettingsComponent } from '../settings/settings.component';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { database } from 'src/environments/environment';
 
 @Component({
