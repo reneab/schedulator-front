de5271838c51c2ca927ee9c02e2b0c20cff025b8
reneab <rene.abd@gmail.com>
2019-08-29 15:46:55 +0800

    fix tests

--- src/app/schedule-firestore.service.spec.ts
+++ src/app/schedule-firestore.service.spec.ts
@@ -1,9 +1,17 @@
 import { TestBed } from '@angular/core/testing';
-
+import { AngularFirestore } from '@angular/fire/firestore';
 import { ScheduleFirestoreService } from './schedule-firestore.service';
 
-describe('ScheduleFirestoreService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+const AngularFirestoreStub = {
+  collection: (name) => [{ to: new Date(), from: new Date(), title: 'No one cares' }]
+};
+
+describe('SettingsFirestoreService', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      { provide: AngularFirestore, useValue: AngularFirestoreStub }
+    ]
+  }));
 
   it('should be created', () => {
     const service: ScheduleFirestoreService = TestBed.get(ScheduleFirestoreService);
--- src/app/settings-firestore.service.spec.ts
+++ src/app/settings-firestore.service.spec.ts
@@ -1,9 +1,18 @@
 import { TestBed } from '@angular/core/testing';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 import { SettingsFirestoreService } from './settings-firestore.service';
 
+const AngularFirestoreStub = {
+  collection: (name) => [{ to: new Date(), from: new Date(), title: 'No one cares' }]
+};
+
 describe('SettingsFirestoreService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      { provide: AngularFirestore, useValue: AngularFirestoreStub }
+    ]
+  }));
 
   it('should be created', () => {
     const service: SettingsFirestoreService = TestBed.get(SettingsFirestoreService);
