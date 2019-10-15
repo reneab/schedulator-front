1ae9c77a5070efad8f28835f6869beb88ed3cf3f
reneab <rene.abd@gmail.com>
2019-10-15 21:42:03 +0800

    recommit environment variables

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,17 @@
+export const environment = {
+    production: true,
+    VERSION: require('../../package.json').version,
+    firebase: {
+        apiKey: 'AIzaSyAX2Qgm27HmSNIbmfLyJ5ATpadX9efupiU',
+        authDomain: 'schedule-master-80cba.firebaseapp.com',
+        databaseURL: 'https://schedule-master-80cba.firebaseio.com',
+        projectId: 'schedule-master-80cba',
+        storageBucket: '',
+        messagingSenderId: '745655924757',
+        firestore: {
+            settingsCollection: 'settings',
+            settingsDocument: 'main',
+            schedulesCollection: 'schedules'
+        }
+    }
+};
